3838267a73968ba24c8b1c402924916dcf2d75c1
Nick Moore <moorenickc@protonmail.com>
2024-09-24 13:02:25 +0000

    chore: migrate build to gitpod and update various settings

--- cheshire_captures_backend/urls.py
+++ cheshire_captures_backend/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from cheshire_captures_backend.views import root_route 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('photographers.urls')),
-    path('auth/', include('dj_rest_auth.urls')), 
-    path('', include('photographers.urls')),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('', root_route, name='root'), 
 ]
--- cheshire_captures_backend/views.py
+++ cheshire_captures_backend/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-def home_view(request):
-    return HttpResponse("Welcome to Cheshire Captures - A Platform for Photographers!")
+@api_view()
+def root_route(request):
+    return Response({
+        "message": "Welcome to Cheshire Captures backend"
+    })
