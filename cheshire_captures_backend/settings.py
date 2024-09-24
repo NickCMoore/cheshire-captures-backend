17787d183f0c55e1d5c260f58d192384240d017a
Nick Moore <moorenickc@protonmail.com>
2024-09-24 22:08:33 +0000

    feat: add API documentation with drf-yasg

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -76,7 +76,8 @@ INSTALLED_APPS = [
     'allauth', 
     'allauth.account',  
     'allauth.socialaccount',  
-    'dj_rest_auth.registration',  
+    'dj_rest_auth.registration',
+    'drf_yasg',
 ]
 
 SITE_ID = 1
