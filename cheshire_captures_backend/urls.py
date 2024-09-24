2e16c5e989ca1c40ba73fbbc8ee3ebf2416d1eb6
Nick Moore <moorenickc@protonmail.com>
2024-09-24 13:17:06 +0000

    feat: Configure dj_rest_auth Endpoints for Login, Logout, and Registration

--- cheshire_captures_backend/urls.py
+++ cheshire_captures_backend/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('photographers.urls')),
     path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', root_route, name='root'), 
 ]
