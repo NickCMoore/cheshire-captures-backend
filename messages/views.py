12744890f366c99c18ecdd8e129430388eb78ea0
Nick Moore <moorenickc@protonmail.com>
2024-09-27 20:15:12 +0000

    build: add serializers and views for messages

--- messages/views.py
+++ messages/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticated
+from .models import Message
+from .serializers import MessageSerializer
+from photographers.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user.photographer
+        return Message.objects.filter(sender=user) | Message.objects.filter(recipient=user)
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user.photographer)
