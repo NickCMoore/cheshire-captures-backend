fe614cfe635a6a2b5d3af2671af2ec123ce1c147
Nick Moore <moorenickc@protonmail.com>
2024-09-27 09:50:10 +0000

    build: migrate and update serializer import for Comment model

--- photo/serializers.py
+++ photo/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Photo, Tag, Like
+from .models import Photo, Tag, Like, Comment
 
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
