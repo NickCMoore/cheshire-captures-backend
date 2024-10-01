61e7fb4b4d8c0d2b5538c34b1b9d06e3399327fd
Nick Moore <moorenickc@protonmail.com>
2024-10-01 12:39:21 +0000

    fix: update settings to fix deployment issue with Rest Auth Serializers

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -126,7 +126,7 @@ JWT_AUTH_REFRESH_COOKIE = 'cheshire-captures-refresh'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'dj_rest_auth.serializers.UserDetailsSerializer'
 }
 
 # Email Configuration
