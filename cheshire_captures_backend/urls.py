559aa6d4e64d79d4559d99c17c59f63596cc46cd
Nick Moore <moorenickc@protonmail.com>
2024-10-04 19:57:03 +0000

    fix: remove additional logout from url patterns

--- cheshire_captures_backend/urls.py
+++ cheshire_captures_backend/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),  
-    path('api/dj-rest-auth/logout/'),  
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),  
     path('api/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')), 
     
