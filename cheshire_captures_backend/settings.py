88425e923fbddde76d7777d4c15924ef992f2c82
Nick Moore <moorenickc@protonmail.com>
2024-10-04 19:23:48 +0000

    fix: add back in CORS settings

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -38,6 +38,21 @@ ALLOWED_HOSTS = [
     '8000-nickcmoore-cheshirecapt-1t388js0qvn.ws-eu116.gitpod.io',
 ]
 
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
