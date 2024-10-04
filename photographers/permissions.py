f53a9b4b5824e5c15eb01463a9b8422bfe28a187
Nick Moore <moorenickc@protonmail.com>
2024-10-04 19:30:29 +0000

    feat: update permissions for users

--- photographers/permissions.py
+++ photographers/permissions.py
@@ -1,13 +1,15 @@
 from rest_framework import permissions
-from messaging.models import Message 
+from messaging.models import Message
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
 
+        if not request.user.is_authenticated:
+            return False
+
         if isinstance(obj, Message):
             return obj.sender.user == request.user
 
         return obj.user == request.user
-
