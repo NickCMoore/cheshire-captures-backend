22d27f7bbf0e189442cd79e87fcb2376e38ab56b
Nick Moore <moorenickc@protonmail.com>
2024-10-04 19:22:00 +0000

    chore: update allowed hosts in settings

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 import os
 import dj_database_url
-import re
 import logging
-from corsheaders.defaults import default_headers
 
 # Load environment variables from env.py if it exists
 if os.path.exists('env.py'):
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 logger = logging.getLogger('django.security.DisallowedHost')
 logger.addHandler(logging.StreamHandler())
@@ -35,9 +33,9 @@ ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     '127.0.0.1',
     'localhost',
-    '*.gitpod.io', 
-    '8000-nickcmoore-cheshirecapt-1t388js0qvn.ws-eu116.gitpod.io',
+    '*.gitpod.io',
     'cheshire-captures-4a500dc7ab0a.herokuapp.com',
+    '8000-nickcmoore-cheshirecapt-1t388js0qvn.ws-eu116.gitpod.io',
 ]
 
 INSTALLED_APPS = [
@@ -61,7 +59,6 @@ INSTALLED_APPS = [
     'drf_yasg',
     'photo',
     'messaging',
-    'corsheaders',
 ]
 
 SITE_ID = 1
@@ -73,11 +70,7 @@ AUTHENTICATION_BACKENDS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        (
-            'rest_framework.authentication.SessionAuthentication'
-            if 'DEV' in os.environ
-            else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-        )
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
@@ -91,32 +84,14 @@ JWT_AUTH_REFRESH_COOKIE = 'cheshire-captures-refresh'
 JWT_AUTH_SAMESITE = 'None'
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
-    ).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
-
-CORS_ALLOW_CREDENTIALS = True
-
 
 ROOT_URLCONF = 'cheshire_captures_backend.urls'
 
@@ -147,8 +122,5 @@ TEMPLATES = [
         },
     },
 ]
-
-
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
