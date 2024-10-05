9b2e713c9e002c37f167a8f04ddd730b67285a6c
Nick Moore <142320725+NickCMoore@users.noreply.github.com>
2024-10-05 09:55:58 +0100

    chore: remove views.py from root folder

--- cheshire_captures_backend/urls.py
+++ cheshire_captures_backend/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import root_route
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,6 +33,4 @@ urlpatterns = [
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    # Fallback route to the root route view
-    re_path(r'^.*$', root_route, name='root'), 
 ]
