5b44751ce707e841d94a6ba29c87e1a5ba3c655c
Nick Moore <moorenickc@protonmail.com>
2024-10-09 13:36:07 +0000

    chore: update admin.pys to view new models in admin panel

--- photo/admin.py
+++ photo/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo, Tag, Like, Comment
 
 admin.site.register(Photo)
+admin.site.register(Tag)
+admin.site.register(Like)
+admin.site.register(Comment)
--- photographers/admin.py
+++ photographers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Photographer
+from .models import Photographer, Follow
 
+# Register your models here
 admin.site.register(Photographer)
+admin.site.register(Follow)
