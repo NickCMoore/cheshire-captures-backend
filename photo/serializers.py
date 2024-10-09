78380536dfee478155f19fba227a69dbf5f8ee62
Nick Moore <moorenickc@protonmail.com>
2024-10-09 11:24:34 +0000

    further updates to serializers photo

--- photo/serializers.py
+++ photo/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Photo
+from .models import Photo, Tag, Like, Comment
 
 class PhotoSerializer(serializers.ModelSerializer):
     photographer_display_name = serializers.SerializerMethodField()
@@ -18,3 +18,23 @@ class PhotoSerializer(serializers.ModelSerializer):
             'description', 'image_url', 'category', 'tags', 'created_at', 'updated_at'
         ]
         read_only_fields = ['photographer', 'created_at', 'updated_at']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ['id', 'photographer', 'photo', 'created_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    photographer = serializers.ReadOnlyField(source='photographer.user.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'photographer', 'photo', 'content', 'created_at', 'updated_at']
