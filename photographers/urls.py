0dd53cae1621809850a859f8695446b60dc04ac5
Nick Moore <moorenickc@protonmail.com>
2024-10-10 21:18:09 +0000

    fix: update urlpattern for photographers to avoid positional argument error #19

--- photographers/urls.py
+++ photographers/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PhotographerDetail, PhotographerList, FollowViewSet, TopPhotographersView
+from photographers.views import (
+    PhotographerDetail, 
+    PhotographerList, 
+    TopPhotographersView, 
+    FollowViewSet
+)
 
+# Using a router for the FollowViewSet
 router = DefaultRouter()
 router.register(r'follows', FollowViewSet, basename='follow')
 
