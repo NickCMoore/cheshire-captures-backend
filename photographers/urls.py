81c943b1b686d05005ce5addd49ea4a727c03cc8
Nick Moore <moorenickc@protonmail.com>
2024-10-10 21:14:25 +0000

    feat: update photographers url patterns to include follow #19

--- photographers/urls.py
+++ photographers/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from photographers.views import (
-    PhotographerDetail, 
-    PhotographerList, 
-    TopPhotographersView, 
-    FollowViewSet
-)
+from .views import PhotographerDetail, PhotographerList, FollowViewSet, TopPhotographersView
 
-# Using a router for the FollowViewSet
 router = DefaultRouter()
 router.register(r'follows', FollowViewSet, basename='follow')
 
 urlpatterns = [
     path('photographers/', PhotographerList.as_view(), name='photographer-list'),
-    path('photographers/<int:pk>/', PhotographerDetail.as_view(), name='photographer-detail'),
     path('photographers/top/', TopPhotographersView.as_view(), name='top-photographers'),
-    path('', include(router.urls)),  
+    path('photographers/<int:pk>/', PhotographerDetail.as_view(), name='photographer-detail'),
+    path('photographers/<int:pk>/follow/', PhotographerDetail.as_view({'post': 'follow'}), name='photographer-follow'),
+    path('photographers/<int:pk>/unfollow/', PhotographerDetail.as_view({'post': 'unfollow'}), name='photographer-unfollow'),
+    path('', include(router.urls)),
 ]
