b92b0d0a189c00aaeb070933454d8e513dece67c
Nick Moore <moorenickc@protonmail.com>
2024-10-11 18:58:11 +0000

    fix: update REST settings to remove session authentication temporarily

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -60,11 +60,9 @@ SITE_ID = 1
 
 # REST framework settings
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [(
-        'rest_framework.authentication.SessionAuthentication'
-        if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )],
+'DEFAULT_AUTHENTICATION_CLASSES': [
+    'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
+],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
