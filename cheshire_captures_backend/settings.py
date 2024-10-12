603c9c32ac2bd4d6d8a4d6a1ebc29327b464b521
Nick Moore <moorenickc@protonmail.com>
2024-10-12 11:58:24 +0000

    fix: update urls for unfollows

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -66,9 +66,7 @@ SITE_ID = 1
 # REST framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication'
-        if 'DEV' in os.environ
-        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
--- photographers/urls.py
+++ photographers/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('photographers/', PhotographerList.as_view(), name='photographer-list'),
     path('photographers/<int:pk>/', PhotographerDetail.as_view(), name='photographer-detail'),
     path('photographers/<int:pk>/follow/', follow_photographer, name='photographer-follow'),
+    path('photographers/<int:pk>/unfollow/', PhotographerViewSet.as_view({'post': 'unfollow'}), name='photographer-unfollow'),
 ]
