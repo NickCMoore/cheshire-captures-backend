1e96bf11621b3debe8686a3b97a318d6c96d1026
Nick Moore <moorenickc@protonmail.com>
2024-10-12 12:19:48 +0000

    add PhotographerViewSet to import in urls.py

--- photographers/urls.py
+++ photographers/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PhotographerList, PhotographerDetail, follow_photographer
+from .views import PhotographerList, PhotographerDetail, follow_photographer, PhotographerViewSet
 
 urlpatterns = [
     path('photographers/', PhotographerList.as_view(), name='photographer-list'),
