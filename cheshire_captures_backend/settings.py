a4c6f5cba2621eb7c0fd8488f158c5dc349a6e51
Nick Moore <moorenickc@protonmail.com>
2024-10-14 02:07:01 +0000

    update error logging in settings

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -68,6 +68,7 @@ REST_FRAMEWORK = {
 # Use JSON renderer only in production
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
     ]
 
@@ -120,6 +121,7 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+DEBUG = True
 
 # CSRF settings
 CSRF_TRUSTED_ORIGINS = [
@@ -190,3 +192,17 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
