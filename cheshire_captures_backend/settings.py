927cd8bf6637495297c5e33669df5e336d536ba8
Nick Moore <moorenickc@protonmail.com>
2024-11-28 14:59:39 +0000

    update JWT access and refresh token expiries

--- cheshire_captures_backend/settings.py
+++ cheshire_captures_backend/settings.py
@@ -199,6 +199,8 @@ JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+JWT_AUTH_REFRESH_TOKEN_LIFETIME = timedelta(days=7)
+JWT_AUTH_ACCESS_TOKEN_LIFETIME = timedelta(minutes=20)
 
 REST_AUTH_SERIALIZERS = {
     'USER_DETAILS_SERIALIZER': 'cheshire_captures_backend.serializers.CurrentUserSerializer',
