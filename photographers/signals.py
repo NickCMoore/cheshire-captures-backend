b9497f78cfe8a585983993ec8776b4272cc2698d
Nick Moore <moorenickc@protonmail.com>
2024-12-02 12:09:57 +0000

    update signal for photographer

--- photographers/signals.py
+++ photographers/signals.py
@@ -1,15 +1,17 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Photographer
 from django.contrib.auth.models import User
+from .models import Photographer
 
 @receiver(post_save, sender=User)
-def create_user_photographer(sender, instance, created, **kwargs):
-    """Create a photographer for a new user."""
+def create_photographer(sender, instance, created, **kwargs):
+    """Create a Photographer for a new User."""
     if created:
         Photographer.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_photographer(sender, instance, **kwargs):
-    """Save the photographer for an existing user."""
-    instance.photographer.save()
+def save_photographer(sender, instance, **kwargs):
+    """Save the Photographer when the User is saved."""
+    if hasattr(instance, 'photographer'):
+        instance.photographer.save()
+S
