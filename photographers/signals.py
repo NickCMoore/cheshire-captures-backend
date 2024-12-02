cd7ffb30a573f0807b4c42feee0a206bea0f4317
Nick Moore <moorenickc@protonmail.com>
2024-12-02 11:57:48 +0000

    fix missing import in signals.py

--- photographers/signals.py
+++ photographers/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import Profile, Notification
+from .models import Photographer, Notification
 from django.contrib.auth.models import User
 
 @receiver(post_save, sender=User)
